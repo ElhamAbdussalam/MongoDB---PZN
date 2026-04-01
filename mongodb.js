d23d51e0b6961e4cbf2158509c283293ae1fe98d
ElhamAbdussalam <muhammadelhamcity7@gmail.com>
2026-04-01 14:28:40 +0700

    query modifier

--- mongodb.js
+++ mongodb.js
@@ -255,3 +255,14 @@ db.products.find(
     },
   },
 );
+
+db.products.find({}).count();
+
+db.products.find({}).limit(4);
+
+db.products.find({}).limit(4).skip(2);
+
+db.products.find({}).sort({
+  category: 1,
+  name: -1,
+});
