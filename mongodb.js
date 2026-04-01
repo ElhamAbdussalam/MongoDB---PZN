d99442ec212eafb8f273c545c91d0c3e034a331c
ElhamAbdussalam <muhammadelhamcity7@gmail.com>
2026-04-01 14:20:02 +0700

    projection opertor

--- mongodb.js
+++ mongodb.js
@@ -201,3 +201,57 @@ db.products.find({
     $size: 3,
   },
 });
+
+db.products.find(
+  {},
+  {
+    name: 1,
+    category: 1,
+  },
+);
+
+db.products.find(
+  {},
+  {
+    tags: 0,
+    price: 0,
+  },
+);
+
+db.products.find(
+  {},
+  {
+    name: 1,
+    tags: {
+      $elemMatch: {
+        $in: ["samsung", "logitech", "accessories"],
+      },
+    },
+  },
+);
+
+db.products.find(
+  {
+    tags: {
+      $exists: true,
+    },
+  },
+  {
+    name: 1,
+    "tags.$": 1,
+  },
+);
+
+db.products.find(
+  {
+    tags: {
+      $exists: true,
+    },
+  },
+  {
+    name: 1,
+    tags: {
+      $slice: 2,
+    },
+  },
+);
