11e339b07a94ce3179f84f14ba2bf65fd2e89afd
ElhamAbdussalam <muhammadelhamcity7@gmail.com>
2026-04-01 08:09:39 +0700

    Inser Document

--- mongodb.js
+++ mongodb.js
@@ -5,3 +5,40 @@ db.createCollection("products");
 db.createCollection("orders");
 
 db.getCollectionNames();
+
+db.customers.find();
+
+db.customers.insertOne({
+  _id: "elham",
+  name: "M Elham Abdussalam",
+});
+
+db.products.insertMany([
+  {
+    _id: 1,
+    name: "Indomie Ayam Bawang",
+    price: new NumberLong("2000"),
+  },
+  {
+    _id: 2,
+    name: "Mie Sedap Soto",
+    price: new NumberLong("2000"),
+  },
+]);
+
+db.orders.insertOne({
+  _id: new ObjectId(),
+  total: new NumberLong("8000"),
+  items: [
+    {
+      product_id: 1,
+      price: new NumberLong("2000"),
+      quantity: new NumberInt("2"),
+    },
+    {
+      product_id: 2,
+      price: new NumberLong("2000"),
+      quantity: new NumberInt("2"),
+    },
+  ],
+});
