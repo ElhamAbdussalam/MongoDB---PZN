6bef83e9b232efd940c09e5fb305ea1bffd17a69
ElhamAbdussalam <muhammadelhamcity7@gmail.com>
2026-04-02 13:35:50 +0700

    delete document

--- mongodb.js
+++ mongodb.js
@@ -358,3 +358,33 @@ db.products.updateMany(
     },
   },
 );
+
+db.customers.insertOne({
+  _id: "spammer",
+  full_name: "Spammer",
+});
+
+db.customers.deleteOne({
+  _id: "spammer",
+});
+
+db.customers.insertMany([
+  {
+    _id: "spammer1",
+    full_name: "Spammer1",
+  },
+  {
+    _id: "spammer2",
+    full_name: "Spammer2",
+  },
+  {
+    _id: "spammer3",
+    full_name: "Spammer3",
+  },
+]);
+
+db.customers.deleteMany({
+  _id: {
+    $regex: "spammer",
+  },
+});
